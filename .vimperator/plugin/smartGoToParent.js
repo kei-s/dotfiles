f0743a3a5184d974d9e2251bcd61851e956a132e
Kei Shiratsuchi <kei-s@mbc.nifty.com>
2010-12-21 16:43:39 +0900

    for vimperator

--- .vimperator/plugin/smartGoToParent.js
+++ .vimperator/plugin/smartGoToParent.js
@@ -7,7 +7,7 @@ var PLUGIN_INFO =
 <version>0.1</version>
 <minVersion>2.3</minVersion>
 <maxVersion>2.4</maxVersion>
-<updateURL></updateURL>
+<updateURL>http://gist.github.com/raw/604592/smartGoToParent.js</updateURL>
 <detail lang="ja"><![CDATA[
 Uppity風に賢く上位ディレクトリに移動するプラグイン
 == Command ==
@@ -43,57 +43,61 @@ EOF
 </VimperatorPlugin>;
 (function(){
   // thanks to http://trac.arantius.com/browser/extension/uppity/content/uppity.js
-  let regexp = new RegExp('([a-z]+://)([^/]*)(/.*)');
-  let goUpPath = function(path) {
-    if (!path) return;
+  function getParent(url,count) {
+    function getParentPath(path) {
+      if (!path) return;
+        path = path.replace(/\/$/,'').replace(/^\/+/,'');
+      if (path.indexOf('#')>0) {
+        return path.replace(/#.*/,'');
+      }
+      if (path.indexOf('?')>0) {
+        return path.replace(/\?.*/,'');
+      }
+      path = path.replace(/\/+$/,'');
+      if (path.indexOf('/')>0) {
+        return path.replace(/\/[^\/]*$/,'/');
+      }
+    }
+    function getParentHost(host) {
+      if (!/\./.test(host) || /^[0-9+.:]+$/.test(host)){
+        return host;
+      }
+      let hostSuffix='';
+      let x=host.lastIndexOf(':');
+      if (x>0) {
+        hostSuffix = host.substr(x);
+        host = host.substr(0,x);
+      }
+      hostSuffix = host.substr(host.length-6)+hostSuffix;
+      host = host.substr(0,host.length-6);
+      return host.replace(/[^.]*\./,'')+hostSuffix;
+    }
+    let parent = url;
+    let regexp = new RegExp('([a-z]+:///?)([^/]*)(/.*)');
+    let [,scheme,host,path] = regexp.exec(url);
     path = path.replace(/\/$/,'').replace(/^\/+/,'');
-    if (path.indexOf('#')>0) {
-      return path.replace(/#.*/,'');
+    for (let i = 0; i < count; i++) {
+      if (path) {
+        if (path = getParentPath(path)) {
+          parent = scheme+host+'/'+path;
+        }
+        else {
+          parent = scheme+host+'/';
+        }
+      }
+      else {
+        host = getParentHost(host);
+        parent = scheme+host+'/';
+      }
     }
-    if (path.indexOf('?')>0) {
-      return path.replace(/\?.*/,'');
-    }
-    path = path.replace(/\/+$/,'');
-    if (path.indexOf('/')>0) {
-      return path.replace(/\/[^\/]*$/,'/');
-    }
-  }
-  let goUpHost = function(host) {
-    if (/^[0-9+.:]+$/.test(host)){
-      return host;
-    }
-    let hostSuffix='';
-    let x=host.lastIndexOf(':');
-    if (x>0) {
-      hostSuffix = host.substr(x);
-      host = host.substr(0,x);
-    }
-    hostSuffix = host.substr(host.length-6)+hostSuffix;
-    host = host.substr(0,host.length-6);
-    return host.replace(/[^.]*\./,'')+hostSuffix;
+    return parent;
   }
   commands.addUserCommand(["goParent"], 'Smart Go to parent',
     function(arg){
       let count = arg.count;
       if (count < 1)
         count = 1;
-      let url = buffer.URL;
-      let [all,scheme,host,path] = regexp.exec(url);
-      path = path.replace(/\/$/,'').replace(/^\/+/,'');
-      for (let i = 0; i < count; i++) {
-        if (path) {
-          if (path = goUpPath(path)) {
-            url = scheme+host+'/'+path;
-          }
-          else {
-            url = scheme+host+'/';
-          }
-        }
-        else {
-          host = goUpHost(host);
-          url = scheme+host+'/';
-        }
-      }
+      let url = getParent(buffer.URL,count);;
       if (url == buffer.URL) {
         liberator.beep();
       }
