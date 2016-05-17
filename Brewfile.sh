8bd9c3706443b7164f04151cc674df9011b7d174
kei-s <kei.shiratsuchi@gmail.com>
2016-05-17 11:34:09 +0900

    sort

--- Brewfile.sh
+++ Brewfile.sh
@@ -14,69 +14,69 @@ brew install zsh-completions
 # sudo sh -c "echo '/usr/local/bin/zsh' >> /etc/shells"
 # chsh -s /usr/local/bin/zsh
 
-brew install coreutils
-brew install git
-brew install tig
-brew install curl --with-rtmp
-brew install lv
-brew install wget
-brew install mysql
-brew install postgresql
-brew install redis
-brew install vim --with-lua
-brew install imagemagick
-brew install qt
-brew install nodejs
-brew install go
-brew install peco
-brew install mercurial
-brew install elasticsearch
-brew install hub
-brew install phantomjs
-brew install tree
-brew install nkf
-brew install jq
-brew install terminal-notifier
-brew install source-highlight
 brew install ansible
-brew install shellcheck
-brew install neovim/neovim/neovim
-brew install optipng
-brew install jpegoptim
-brew install rtmpdump
-brew install rbenv
-brew install pyenv
-brew install homebrew/binary/ngrok2
-brew install osx-cross/avr/avr-libc
+brew install coreutils
+brew install curl --with-rtmp
 brew install dfu-programmer
-brew install mecab
+brew install elasticsearch
+brew install git
 brew install gnupg
+brew install go
 brew install graphviz
+brew install homebrew/binary/ngrok2
+brew install hub
+brew install imagemagick
+brew install jpegoptim
+brew install jq
+brew install lv
+brew install mecab
+brew install mercurial
+brew install mysql
+brew install neovim/neovim/neovim
+brew install nkf
+brew install nodejs
+brew install optipng
+brew install osx-cross/avr/avr-libc
+brew install peco
+brew install phantomjs
+brew install postgresql
+brew install pyenv
+brew install qt
+brew install rbenv
+brew install redis
+brew install rtmpdump
+brew install shellcheck
+brew install source-highlight
+brew install terminal-notifier
+brew install tig
+brew install tree
+brew install vim --with-lua
+brew install wget
 
 #brew cask install alfred
+#brew cask install anvil
+#brew cask install appcleaner
+#brew cask install atom
+#brew cask install blink1control
+#brew cask install butter
+#brew cask install cyberduck
+#brew cask install dropbox
 #brew cask install google-chrome
 #brew cask install google-japanese-ime
-#brew cask install iterm2
-#brew cask install karabiner
-#brew cask install slack
-#brew cask install skype
-#brew cask install butter
-#brew cask install mailplane
-#brew cask install kobito
-#brew cask install dropbox
-#brew cask install trailer
-#brew cask install limechat
-#brew cask install sequel-pro
-#brew cask install appcleaner
 #brew cask install heroku-toolbelt
-#brew cask install atom
-#brew cask install anvil
-#brew cask install cyberduck
-#brew cask install silverlight
-#brew cask install vlc
+#brew cask install iterm2
 #brew cask install java
+#brew cask install karabiner
+#brew cask install kobito
+#brew cask install libreoffice
+#brew cask install limechat
+#brew cask install mailplane
+#brew cask install rescuetime
+#brew cask install sequel-pro
+#brew cask install silverlight
+#brew cask install skype
+#brew cask install slack
+#brew cask install trailer
 #brew cask install vagrant
 #brew cask install virtualbox
-#brew cask install rescuetime
-#brew cask install libreoffice
-#brew cask install blink1control
+#brew cask install vlc
